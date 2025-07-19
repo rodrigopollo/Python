44fd5dff1cb9ca7125de17eaeaa8be0f4a0ad777
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-07-19 14:19:42 +0200

    Primeiro programa de aprensentaçao

---
+++ Mundo_1/Boas_Vindas.py
@@ -0,0 +1,7 @@
+# Crie um script Python que leia o nome de uma pessoa e mostre
+# a mensagem de Boas vindas conforme o valor digitado.
+
+
+nome = input('Qual o seu nome?')
+
+print('Bem vindo', nome,'se sinta em casa')
