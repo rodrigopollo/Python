b71d7493fc376066b62d6abfcf70a845b8f30388
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-07-19 14:20:27 +0200

    Ensina a pedir para o usuario inserir a informaçao em vez de voce mesmo setar o valor.

---
+++ Mundo_1/Input.py
@@ -0,0 +1,12 @@
+
+nome = input('Qual o seu nome?')
+idade = input('Qual a sua idade?')
+cidade = input('Aonde voce mora?')
+
+print(nome, idade, cidade)
+
+
+
+#----------------------------------------------------------------------------
+# Input --> Basicamente eh (leia), ou seja quando voce quer q o usuario
+# insira a informaçao como no exemplo acime, voce usa INPUT.
