cbc110afe591c780c1c3eff3cdd7dc5176be8187
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-07-21 18:23:23 +0200

    Aprendendo a trabalhar com DATAS.

---
+++ Mundo_1/Data_De_Nascimento.py
@@ -0,0 +1,10 @@
+
+# Crie um script Python que leia dia, nome e ano de nascimento de uma pessoa
+# e mostre uma mensagem com a data formatada.
+
+dia = input('Qual dia voce nasceu?')
+mes = input('Qual mes voce nasceu?')
+ano = input('Qual ano voce nasceu?')
+
+print(dia + '/' + mes + '/' + ano + '.')
+print(f"Voce nasceu no dia {dia} de {mes} do ano de {ano}")
