c91b694f74f3d71dbe09c1bc2e6b0919dd41ed1c
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-07-21 18:24:30 +0200

    Convertendo temperatura de ºC para F

---
+++ Mundo_1/Graus_Converter.py
@@ -0,0 +1,6 @@
+# Crie um programa que converta graus Celsius para Fahrenheit
+
+temperatura = float(input("Insira a temperatura atual em Cº: "))
+
+conversor = temperatura * 1.8 + 32
+print(f"Hoje faz {temperatura}ºC que equivale a {conversor}ºF")
