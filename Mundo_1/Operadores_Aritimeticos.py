ab2b26cbdb0a87e953a1860d6cbbc9735e9e06c2
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-07-23 10:06:37 +0200

    Operadpres Aritimetricos

---
+++ Mundo_1/Operadores_Aritimeticos.py
@@ -0,0 +1,31 @@
+
+soma = 5+2
+subtracao = 5-2
+multiplicacao = 5*2
+divisao = 4/3
+potencia = 5**2
+divisaoInteira = 5//2
+resto = 5%2
+
+print(f"A soma de 5 + 2 = {soma}")
+print(f"A subtraçao de 5 - 2 = {subtracao}")
+print(f"A multiplicaçao de 5 * 2 = {multiplicacao}")
+print(f"A divisao de 5 / 2 = {divisao:.2f}")
+print(f"A potencia de 5 ** 2 = {potencia}")
+print(f"A divisao inteira de 5 // 2 = {divisaoInteira}")
+print(f"O resto de 5 % 2 = {resto}")
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+"""
+soma -> +
+subtraçao -> -
+multiplicaçao -> *
+divisao -> /
+potencia -> **
+divisao inteira -> //
+resto -> %
+
+NOTA.: Voce pode ver que na linha 13 (DIVISAO) estamos usando -> (:.2f), serve para estabeler
+o numero de casas decimais visiveis, nesse caso seram 2 (ex: 4/4 = 1.33 em vez de 1.33333333)
+
+"""
