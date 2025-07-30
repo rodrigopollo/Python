c6810506fc88827800df3684f2695d17cbb205e8
Rodrigo P. Cardoso <rodrigo.spw20@gmail.com>
2025-07-30 20:54:20 +0200

    Sorteia 4 alunos em ordem aleatoria para saber quem ficou em 1º, 2º e 3º

---
+++ Mundo_1/Sorteio_Ordem.py
@@ -0,0 +1,11 @@
+import random
+
+n1 = "Rodrigo"
+n2 = "Ana"
+n3 = "Roberto"
+n4 = "Susy"
+
+sorteio = [n1, n2, n3, n4]
+random.shuffle(sorteio)
+
+print(f"Sorteio depois do SHUFFLE embaralhar os nomes = {sorteio}")
